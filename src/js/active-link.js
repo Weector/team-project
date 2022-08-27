6ce9751c162a79316a113b4d6350fc3ad2f9bb17
Leonid Holub <rsleonid@gmail.com>
2022-08-27 12:17:09 +0300

    Додає анімацію якорних посилань при скролінгу.

---
+++ src/js/active-link.js
@@ -0,0 +1,38 @@
+var sections = $('section'),
+  nav = $('nav'),
+  header_height = $('.header').outerHeight();
+
+$(window).on('wheel', function () {
+  var cur_pos = $(this).scrollTop();
+
+  sections.each(function () {
+    var top = $(this).offset().top - header_height;
+
+    if (cur_pos >= top) {
+      nav.find('.header-nav-link.current').removeClass('current');
+
+      nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('current');
+    }
+  });
+});
+
+nav.find('.header-nav-link').on('click', function () {
+  var $el = $(this),
+    id = $el.attr('href'),
+    activeLink = $('.header-nav-link.current');
+
+  if (activeLink) {
+    activeLink.removeClass('current');
+  }
+
+  $el.toggleClass('current');
+
+  $('html, body').animate(
+    {
+      scrollTop: $(id).offset().top - header_height,
+    },
+    500
+  );
+
+  return false;
+});
